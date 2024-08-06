0136402cf96e6d204033529e4dc4a4f452c08f52
RobertaMi89 <roberta.miosi@gmail.com>
2024-08-06 14:47:50 +0200

    setting modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,67 @@
-import { useState } from "react";
-import "./App.css";
+import React, { useState } from "react";
 import BackgroundOverlay from "./components/atoms/BackgroundOverlay";
 import Logo from "./components/atoms/Logo";
 import Button from "./components/atoms/Button";
 import IconCarousel from "./components/molecules/IconCarousel";
-import ModalForm from "./components/molecules/ModalForm";
+import LoginModal from "./components/molecules/LoginModal";
+import SignupModal from "./components/molecules/SignUpModal";
+import { labels } from "./components/data/labels";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [activeModal, setActiveModal] = useState(null);
+  const navigate = useNavigate();
 
-  const handleOpenModal = () => {
-    setIsModalVisible(true);
+  const handleOpenLoginModal = () => {
+    setActiveModal("login");
   };
 
   const handleCloseModal = () => {
-    setIsModalVisible(false);
+    setActiveModal(null);
   };
+
+  const handleLoginSuccess = () => {
+    handleCloseModal();
+    navigate("/discover"); // Reindirizza alla pagina Discover dopo il login
+  };
+
+  const handleSignupSuccess = () => {
+    setActiveModal("login"); // Dopo la registrazione, torna alla modale di login
+  };
+
   return (
     <div className="relative min-h-screen flex flex-col">
       <BackgroundOverlay />
-      <div className="relative z-10 w-80 mx-auto flex-grow flex flex-col justify-between ">
+      <div className="relative z-10 w-80 mx-auto flex-grow flex flex-col justify-between">
         <Logo />
         <div>
           <IconCarousel />
         </div>
-        {!isModalVisible && (
+        {!activeModal && (
           <div className="flex justify-center">
             <Button
-              text="Registrati/Accedi"
+              text={labels.signUpLogIn}
               className="bg-yellow-400 rounded-2xl px-4 py-2 mb-5"
-              onClick={handleOpenModal}
+              onClick={handleOpenLoginModal}
             />
           </div>
         )}
       </div>
-      <ModalForm isVisible={isModalVisible} onClose={handleCloseModal} />
+      {activeModal === "login" && (
+        <LoginModal
+          isVisible={true}
+          onClose={handleCloseModal}
+          onLoginSuccess={handleLoginSuccess}
+          onSwitchToSignup={() => setActiveModal("signup")}
+        />
+      )}
+      {activeModal === "signup" && (
+        <SignupModal
+          isVisible={true}
+          onClose={handleCloseModal}
+          onSignupSuccess={handleSignupSuccess}
+        />
+      )}
     </div>
   );
 }
