4986479efe75e6fe8b930ab39b558ebb5d64539f
RobertaMi89 <roberta.miosi@gmail.com>
2024-08-06 14:46:59 +0200

    refactor routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import DefaultLayout from "./components/pages/DefaultLayout.jsx";
 import "./index.css";
+import ErrorPage from "./components/pages/ErrorPage.jsx";
+import Discover from "./components/pages/Discover.jsx";
+import MovieDetailPage from "./components/pages/MovieDetailPage.jsx";
+import SeriesDetailPage from "./components/pages/SeriesDetailPage.jsx";
 import { EndpointsProvider } from "./components/context/EndpointsContext.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { UserProvider } from "./components/context/UserContext.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/discover",
+        element: <Discover />,
+      },
+      {
+        path: "/movie/:id",
+        element: <MovieDetailPage />,
+      },
+      {
+        path: "/series/:id",
+        element: <SeriesDetailPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <EndpointsProvider>
-      <App />
+      <UserProvider>
+        <RouterProvider router={router} />
+      </UserProvider>
     </EndpointsProvider>
   </React.StrictMode>
 );
