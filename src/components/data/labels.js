fef5a3751840634f416297a84b50595d41ffef4d
RobertaMi89 <roberta.miosi@gmail.com>
2024-08-06 14:48:03 +0200

    add labels

---
+++ src/components/data/labels.js
@@ -0,0 +1,33 @@
+export const labels = {
+  like: "Contribuisci a rendere le tue serie preferite ancora miglioriTieni traccia dei tuoi film e delle tue serie TV",
+  heart: "Scopri la tua nuova serie preferita",
+  calendar: "Ricorda il punto in cui avevi interrotto la visione",
+  check: "Tieni traccia dei tuoi film e delle serie tv",
+  bell: "Non perderti nemmeno un episodio",
+
+  signUpLogIn: "Registrati/Accedi",
+
+  logIn: "Accedi",
+  email: "La tua mail",
+  password: "La tua password",
+  checkbox: "Ricorda questo dispositivo",
+  lostPass: "Hai dimenticato la password?",
+  logInAccount: "Accedi al tuo account",
+  notSignUp: "Non sei ancora registrato? ",
+  createAccount: "Crea un account",
+
+  signUp: "Registrati",
+  nome: "Il tuo nome utente",
+
+  search: "Cerca Film e serie TV",
+  cancel: "Annulla",
+  showMore: "Scopri di più",
+  forYouSeries: "Le serie migliori per te",
+  recommendedSeries: "Serie di tendenza",
+  topMovies: "Film di tendenza",
+  browseAllSeries: "Sfoglia tutte le serie",
+  browseAllMovies: "Sfoglia tutti i film",
+
+  visto: "visto",
+  nonVisto: "non visto",
+};
